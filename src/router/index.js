a9893a16b5d01d1351fcf016ac7228e697e39e47
LihaoLixue <lihaox@126.com>
2020-08-09 23:14:59 +0800

    ceshi

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+// 导入刚才编写的组件
+import AppIndex from '@/components/home/AppIndex'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -10,6 +13,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/index',
+      name: 'AppIndex',
+      component: AppIndex
     }
   ]
 })
